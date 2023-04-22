ed366976b091a5a5408a8a2e23153119d74f47cd
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-23 01:04:14 +0300

    add buttun added.

--- toDoListApp/toDoListApp/ViewController.swift
+++ toDoListApp/toDoListApp/ViewController.swift
@@ -7,10 +7,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         tableView.delegate = self
         tableView.dataSource = self
         
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        navigationController?.navigationBar.topItem?.rightBarButtonItem?.tintColor = .white
+        
+    }
+    @objc func addButtonClicked(){
+        performSegue(withIdentifier: "toDetailsVC", sender: self)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -19,7 +25,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         cell.textLabel?.text = "TEST"
-        cell.detailTextLabel?.text = "ToDoList"
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
