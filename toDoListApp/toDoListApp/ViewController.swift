3833e2e59429bf08546c7a8b39cd244261e1da8c
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-23 00:18:17 +0300

    Docs

---
+++ toDoListApp/toDoListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  toDoListApp
+//
+//  Created by Serdar Altındaş on 23.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
