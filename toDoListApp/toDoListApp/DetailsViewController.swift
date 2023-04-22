98da3a7d6adb361afb34aadc589f960cbc837853
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-23 00:43:13 +0300

    All view's created.

---
+++ toDoListApp/toDoListApp/DetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.swift
+//  toDoListApp
+//
+//  Created by Serdar Altındaş on 23.04.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+    
+    @IBOutlet weak var bedenTextField: UITextField!
+    @IBOutlet weak var fiyatTextField: UITextField!
+    @IBOutlet weak var isimTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func kaydetButtonTiklandi(_ sender: UIButton) {
+        
+    }
+    
+}
--- toDoListApp/toDoListApp/ViewController.swift
+++ toDoListApp/toDoListApp/ViewController.swift
@@ -1,18 +1,29 @@
-//
-//  ViewController.swift
-//  toDoListApp
-//
-//  Created by Serdar Altındaş on 23.04.2023.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
         
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "TEST"
+        cell.detailTextLabel?.text = "ToDoList"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetailsVC", sender: self)
     }
 }
 
